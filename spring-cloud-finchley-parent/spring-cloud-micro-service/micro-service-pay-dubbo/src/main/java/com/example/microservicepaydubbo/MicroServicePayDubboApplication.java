f71907c6ea7f03f5d12ab6d29d90ddf9452530bf
zhili yu <yuzl@broada.com>
2019-04-09 17:51:40 +0800

    init micro-service-pay-dubbo service

---
+++ spring-cloud-finchley-parent/spring-cloud-micro-service/micro-service-pay-dubbo/src/main/java/com/example/microservicepaydubbo/MicroServicePayDubboApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicepaydubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicePayDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServicePayDubboApplication.class, args);
+    }
+
+}
